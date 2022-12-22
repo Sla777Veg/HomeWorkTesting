836cbda619ed60b4af1e2c4d4a35eaf9fb85801d
Vyacheslav <viacheslav87klerik@gmail.com>
2022-12-22 06:47:51 +0300

    Commit

---
+++ HomeworkTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworktesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkTestingApplication.class, args);
+    }
+
+}
